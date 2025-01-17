8d5d61dc05eb6cb35745a9c97ee455abfdb08dcc
horace19 <1019horacefung@gmail.com>
2025-01-17 18:45:56 +0800

    Add calendar and link it to javascript

--- script/javascript.js
+++ script/javascript.js
@@ -3,3 +3,18 @@ let surname = "chow";
 
 console.log(name);
 console.log(surname);
+console.log((3 + 2) - 76 * (1 + 1));
+
+let a = 10;
+console.log(a + 9);
+
+const month = document.querySelector(".month-select")
+const monthTitle = document.querySelector(".month-title")
+
+function monthName() {
+    if (month.value === "january") {
+        monthTitle.textContent = "January";
+    } 
+}
+
+month.addEventListener("change", monthName)
